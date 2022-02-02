98ef2239adb0f7215e4c5133c31b92f55994d07a
sangay-phuntsho <sangayphuntsho999@gmail.com>
2022-02-02 20:51:37 +1100

    Committed files

---
+++ newyear/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+import datetime
+
+# Create your views here.
+def index(request):
+    now =datetime.datetime.now()
+    newyear = now.month == 1 and now.day == 1
+    return render(request, "newyear/index.html", {
+        "newyear": newyear
+    })
+
